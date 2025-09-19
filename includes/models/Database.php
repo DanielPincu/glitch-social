dd1d547e86985d472cf0c2223638d27b71cfb497
DanielPincu <ghepin01@easv365.dk>
2025-09-19 10:07:27 +0200

    Database model added

---
+++ includes/models/Database.php
@@ -0,0 +1,24 @@
+<?php
+// includes/models/Database.php
+
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'glitch';
+    private $username = 'root';
+    private $password = '';
+    private $charset = 'utf8';
+
+    public function connect(): PDO {
+        try {
+            $pdo = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name};charset={$this->charset}",
+                $this->username,
+                $this->password
+            );
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("DB Connection failed: " . $e->getMessage());
+        }
+    }
+}
